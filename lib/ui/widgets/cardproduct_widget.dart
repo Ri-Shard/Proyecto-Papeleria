13cd00b3486ff340cacb61488eab1771c2fb5a3a
Ri-Shard <rikelpa2001@gmail.com>
2022-06-27 14:25:36 -0500

    S olucionado Alert Dialog Ventas

--- lib/ui/widgets/cardproduct_widget.dart
+++ lib/ui/widgets/cardproduct_widget.dart
@@ -4,6 +4,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:form_field_validator/form_field_validator.dart';
+import 'package:get/get.dart';
 import 'package:proyecto_papeleria/controller/order_controller.dart';
 
 import '../../model/product_model.dart';
@@ -22,6 +23,7 @@ class CardProduct extends StatefulWidget {
   @override
   _CardProductState createState() => _CardProductState();
 }
+  final _formKey = GlobalKey<FormState>();
 
 class _CardProductState extends State<CardProduct>
     with SingleTickerProviderStateMixin {
@@ -55,7 +57,7 @@ class _CardProductState extends State<CardProduct>
     super.dispose();
     _controller.dispose();
   }
-  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     TextEditingController cantidad = TextEditingController();
@@ -63,18 +65,18 @@ class _CardProductState extends State<CardProduct>
 
     return Transform.scale(
       scale: _scale,
-      child: Form(
-        key: _formKey,
-        child: Card(
+              child: Card(
           elevation: 10.0,
           clipBehavior: Clip.antiAlias,
           color: colorFond.withOpacity(0.5),
           margin: const EdgeInsets.all(12.0),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
           child: GestureDetector(
             onTapDown: _tapDown,
             onTapUp: _tapUp,
             onTap: () {
+
               bool repetido = false;
               for (var item in widget.controller.preorders) {
                 if (item.id == widget.product.id) {
@@ -84,77 +86,108 @@ class _CardProductState extends State<CardProduct>
                 }
               }
               if (!repetido) {
+                
                 showDialog(
                     context: context,
                     builder: (BuildContext context) {
-                      return AlertDialog(
-                          backgroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          title: const Text("AGREGAR"),
-                          content: const Text("Producto Agregado Correctamente"),
-                          actions: [
-                            Row(
-                              children: [
-                                const Text(
-                                  "Cantidad",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w400,
-                                      color: Colors.black87),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                TextField(
-                                  controller: cantidad,
-                                  // validator: (value) {
-                                  //   if (value!.isEmpty) {
-                                  //     return 'La cantidad es Requerida';
-                                  //   } else if (int.parse(value) < 1) {
-                                  //     return 'Valor incorrecto';
-                                  //   } else if (int.parse(value) >
-                                  //       int.parse(widget.product.quantity)) {
-                                  //     return 'Precio Exhorbitante';
-                                  //   }
-                                  //   return null;
-                                  // },
-                                  // autovalidateMode:
-                                  //     AutovalidateMode.onUserInteraction,
-                                  keyboardType: TextInputType.number,
-                                  inputFormatters: <TextInputFormatter>[
-                                    FilteringTextInputFormatter.allow(
-                                        RegExp(r'[0-9]')),
-                                  ],
-                                  obscureText: false,
-                                  decoration: const InputDecoration(
-                                    contentPadding: EdgeInsets.symmetric(
-                                        vertical: 0, horizontal: 10),
-                                    enabledBorder: OutlineInputBorder(
-                                        borderSide:
-                                            BorderSide(color: Colors.grey)),
-                                    border: OutlineInputBorder(
-                                        borderSide:
-                                            BorderSide(color: Colors.grey)),
+                      return Form(
+                        key: _formKey,
+                        child: Container(
+                          alignment: Alignment.center,
+                          margin: const EdgeInsets.all(20),
+                          child: Card(
+                            child: Container(
+                              width: 300,
+                              height: 220,
+                              padding: const EdgeInsets.all(20),
+                              child: Column(
+                                children: [
+
+                                 const Text("Cantidad de productos en Inventario: "),
+                                 Text(widget.product.quantity),
+                                                                 const SizedBox(
+                                    height: 5,
                                   ),
-                                ),
-                              ],
+                                  const Text(
+                                    "Cantidad para Agregar a la Preorden",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.black87),
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  TextFormField(
+                                    controller: cantidad,
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'La cantidad es Requerida';
+                                      } else if (int.parse(value) < 1) {
+                                        return 'Valor incorrecto';
+                                      } else if (int.parse(value) >
+                                          int.parse(widget.product.quantity)) {
+                                        return 'Producto Insuficiente';
+                                      }
+                                      return null;
+                                    },
+                                    autovalidateMode:
+                                        AutovalidateMode.onUserInteraction,
+                                    keyboardType: TextInputType.number,
+                                    inputFormatters: <TextInputFormatter>[
+                                      FilteringTextInputFormatter.allow(
+                                          RegExp(r'[0-9]')),
+                                    ],
+                                    obscureText: false,
+                                    decoration: const InputDecoration(
+                                      contentPadding: EdgeInsets.symmetric(
+                                          vertical: 0, horizontal: 10),
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide:
+                                              BorderSide(color: Colors.grey)),
+                                      border: OutlineInputBorder(
+                                          borderSide:
+                                              BorderSide(color: Colors.grey)),
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      if (_formKey.currentState?.validate() ==
+                                          true) {
+                                        final producto = ProductoModel(
+                                            id: widget.product.id,
+                                            name: widget.product.name,
+                                            quantity: cantidad.text,
+                                            category: widget.product.category,
+                                            image: widget.product.image,
+                                            price: widget.product.price);
+                                        widget.controller.preorders.add(producto);
+                                        Navigator.pop(context);
+                                      } else {
+                                        showDialog(
+                                            context: context,
+                                            builder: (BuildContext context) {
+                                              return AlertDialog(
+                                                backgroundColor: Colors.white,
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(20),
+                                                ),
+                                                title: const Text("ERROR"),
+                                                content: const Text(
+                                                    "Error, Campos vacios"),
+                                              );
+                                            });
+                                      }
+                                    },
+                                    child: const Text('Ok'),
+                                  ),
+                                ],
+                              ),
                             ),
-                            TextButton(
-                              onPressed: () {
-                                final producto = ProductoModel(
-                                    id: widget.product.id,
-                                    name: widget.product.name,
-                                    quantity: cantidad.text,
-                                    category: widget.product.category,
-                                    image: widget.product.image,
-                                    price: widget.product.price);
-                               widget.controller.preorders.add(producto);
-                              },
-                              child: const Text('Ok'),
-                            ),
-                          ]);
+                          ),
+                        ),
+                      );
                     });
               } else {
                 showDialog(
@@ -170,7 +203,9 @@ class _CardProductState extends State<CardProduct>
                               "El producto ya se encuentra Agregado a la Preorden"),
                           actions: [
                             TextButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
                               child: const Text('Ok'),
                             ),
                           ]);
@@ -203,7 +238,8 @@ class _CardProductState extends State<CardProduct>
                                   color: widget.colorAccent.withOpacity(0.7)),
                               Text(widget.product.name,
                                   style: TextStyle(
-                                      color: widget.colorAccent.withOpacity(0.5),
+                                      color:
+                                          widget.colorAccent.withOpacity(0.5),
                                       fontSize: 14,
                                       fontWeight: FontWeight.bold)),
                             ],
@@ -219,7 +255,7 @@ class _CardProductState extends State<CardProduct>
             ),
           ),
         ),
-      ),
+      
     );
   }
 
